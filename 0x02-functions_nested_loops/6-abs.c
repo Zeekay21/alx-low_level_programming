6d554510b6bd17c22ff9b82cd3ea0382655c82c3
Zeekay21 <132398575+Zeekay21@users.noreply.github.com>
2023-06-28 07:37:57 -0700

    function that prints the sign of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the int to check
+ * Return: 0
+ */
+int main(void)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
