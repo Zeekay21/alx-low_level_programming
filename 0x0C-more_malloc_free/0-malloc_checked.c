26e7ba8a3d4b70377f9aaaa9b2637d167d519598
Zeekay21 <132398575+Zeekay21@users.noreply.github.com>
2023-07-20 11:23:05 -0700

    function that allocates memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,14 +1,16 @@
-#include "function_pointers.h"
-#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 /**
-* print_name - print name using pointer to function
-* @name: string to add
-* @f: pointer to function
-* Return: nothing
-**/
-void print_name(char *name, void (*f)(char *))
+* *malloc_checked - allocates memory using malloc
+* @b: number of bytes to allocate
+*
+* Return: a pointer to the allocated memory
+*/
+void *malloc_checked(unsigned int b)
 {
-if (name == NULL || f == NULL)
-return;
-f(name);
+void *ptr;
+ptr = malloc(b);
+if (ptr == NULL)
+exit(98);
+return (ptr);
 }
