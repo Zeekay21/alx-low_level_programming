51be3c98dd611a9ec36ec1ab783ff6d83b77f8f7
Zeekay21 <132398575+Zeekay21@users.noreply.github.com>
2023-08-14 04:54:33 -0700

    function that returns the number of bits you would need to flip to get from one number to another

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * flip_bits - Calculates the count of differing bits needed to transform one number into another
+ * flip_bits - Calculates the count of differing bits needed
+ * to transform one number into another
  * @n: The first number
  * @m: The second number
  *
